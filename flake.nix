6c8f495a8007f90f093ad226fbaf65d9725dcf5d
Fern Garden <mail@fern.garden>
2025-07-03 20:23:08 +0800

    Add firefox-syncserver

--- flake.nix
+++ flake.nix
@@ -72,5 +72,35 @@
           }
         ];
       };
+
+      nixosConfigurations.lxc-firefox-syncserver = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+
+        modules = [
+          (nixpkgs + "/nixos/modules/virtualisation/proxmox-lxc.nix")
+
+          { 
+            networking.hostName = "firefox-syncserver";
+
+            services.mysql.package = nixpkgs.legacyPackages.${system}.mariadb;
+
+            services.firefox-syncserver = {
+              enable = true;
+              secrets = ./firefox-syncserver.env;
+              settings.host = "0.0.0.0";
+              singleNode = {
+                enable = true;
+                hostname = "0.0.0.0";
+                url = "https://fxsync.fern.garden";
+                capacity = 1;
+              };
+            };
+
+            networking.firewall.allowedTCPPorts = [ 5000 ];
+
+            system.stateVersion = "25.05";
+          }
+        ];
+      };
     };
 }
