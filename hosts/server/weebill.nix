136c8bd5acc38114f229e9f175ce60057c426ac5
fern <mail@fern.garden>
2025-07-09 18:46:39 +0800

    revert e95a75657a96ae74f33b972725f7da1e650684da
    revert Raspberry pi config from wiki

--- hosts/server/weebill.nix
+++ hosts/server/weebill.nix
@@ -2,10 +2,6 @@
 {
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    kernelParams = [
-      "snd_bcm2835.enable_hdmi=1"
-      "snd_bcm2835.enable_headphones=1"
-    ];
     initrd.availableKernelModules = [
       "xhci_pci"
       "usbhid"
@@ -14,9 +10,6 @@
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
-      raspberryPi.firmwareConfig = ''
-        dtparam=audio=on
-      '';
     };
   };
 
@@ -27,28 +20,6 @@
       options = [ "noatime" ];
     };
   };
-
-  # Config from <https://nixos.wiki/wiki/NixOS_on_ARM/Raspberry_Pi_4>
-  hardware = {
-    raspberry-pi."4" = {
-      apply-overlays-dtmerge.enable = true;
-      fkms-3d.enable = true;
-    };
-
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
-    };
-  };
-
-  console.enable = false;
-
-  environment.systemPackages = with pkgs; [
-    libraspberrypi
-    raspberrypi-eeprom
-  ];
-
-  networking.networkmanager.wifi.powersave = false; # Prevent host becoming unreachable on wifi after some time.
 
   # Open ports for DHCP server.
   networking.firewall.allowedUDPPorts = [
