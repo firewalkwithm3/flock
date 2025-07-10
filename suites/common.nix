67863cc0771a0668c87d8fb6d93a6426613c3e6e
Fern Garden <mail@fern.garden>
2025-07-10 16:17:49 +0800

    Kanagawa theme for fish shell.

--- suites/common.nix
+++ suites/common.nix
@@ -66,6 +66,7 @@ with lib; {
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
+      # nnn cd on quit.
       function n --wraps nnn --description 'support nnn quit and change directory'
           if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
               echo "nnn is already running"
@@ -85,6 +86,40 @@ with lib; {
               rm -- $NNN_TMPFILE
           end
       end
+
+      # kanagawa theme.
+      set -l foreground DCD7BA normal
+      set -l selection 2D4F67 brcyan
+      set -l comment 727169 brblack
+      set -l red C34043 red
+      set -l orange FF9E64 brred
+      set -l yellow C0A36E yellow
+      set -l green 76946A green
+      set -l purple 957FB8 magenta
+      set -l cyan 7AA89F cyan
+      set -l pink D27E99 brmagenta
+
+      # Syntax Highlighting Colors
+      set -g fish_color_normal $foreground
+      set -g fish_color_command $cyan
+      set -g fish_color_keyword $pink
+      set -g fish_color_quote $yellow
+      set -g fish_color_redirection $foreground
+      set -g fish_color_end $orange
+      set -g fish_color_error $red
+      set -g fish_color_param $purple
+      set -g fish_color_comment $comment
+      set -g fish_color_selection --background=$selection
+      set -g fish_color_search_match --background=$selection
+      set -g fish_color_operator $green
+      set -g fish_color_escape $pink
+      set -g fish_color_autosuggestion $comment
+
+      # Completion Pager Colors
+      set -g fish_pager_color_progress $comment
+      set -g fish_pager_color_prefix $cyan
+      set -g fish_pager_color_completion $foreground
+      set -g fish_pager_color_description $comment
     '';
   };
 
