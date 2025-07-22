a7d2acef9ea97ac07a5f92e2a6d5cd2b5b09605c
Fern Garden <mail@fern.garden>
2025-07-22 22:19:15 +0800

    Nextcloud config

--- hosts/nextcloud/default.nix
+++ hosts/nextcloud/default.nix
@@ -11,12 +11,34 @@
     secrets."nextcloud/admin_pass" = {};
   };
 
+  # Enable Nextcloud.
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud31;
-    hostName = "localhost";
-    config.adminpassFile = config.sops.secrets."nextcloud/admin_pass".path;
-    config.dbtype = "pgsql";
+    hostName = "cloud.ferngarden.net";
+    settings = {
+      trusted_proxies = ["10.0.1.102"];
+      overwriteprotocol = "https";
+    };
+    config = {
+      adminpassFile = config.sops.secrets."nextcloud/admin_pass".path;
+      dbtype = "pgsql";
+    };
     database.createLocally = true;
+    extraApps = {
+      inherit
+        (config.services.nextcloud.package.packages.apps)
+        contacts
+        calendar
+        dav_push
+        end_to_end_encryption
+        gpoddersync
+        user_oidc
+        ;
+    };
+    extraAppsEnable = true;
   };
+
+  # Open port for Nextcloud
+  networking.firewall.allowedTCPPorts = [80];
 }
