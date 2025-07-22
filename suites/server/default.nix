afff1ae299d5604f8cb142e098dd025bbf3e5c1b
Fern Garden <mail@fern.garden>
2025-07-22 10:37:11 +0800

    Add ornithologist to authorized keys

--- suites/server/default.nix
+++ suites/server/default.nix
@@ -18,6 +18,7 @@
   users.users.fern = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETPyuxUVEmYyEW6PVC6BXqkhULHd/RvMm8fMbYhjTMV fern@muskduck"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOmvavslVArcKlBi9a1n26IHBjGqm0NVpqjY2t1PRMp fern@ornithologist"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMoJvPcUJDVVzO4dHROCFNlgJdDZSP5xyPx2s40zcx5QAAAABHNzaDo= YubiKey5NFC"
     ];
   };
