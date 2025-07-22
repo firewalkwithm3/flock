7a771dc49e2db9c1dad263d83f65bf4f4e1406e1
Fern Garden <mail@fern.garden>
2025-07-22 21:07:08 +0800

    Enable nextcloud

--- hosts/nextcloud/default.nix
+++ hosts/nextcloud/default.nix
@@ -4,18 +4,18 @@
   secrets,
   ...
 }: {
-  # # Import secrets.
-  # sops = {
-  #   age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-  #   defaultSopsFile = "${secrets}/sops.yaml";
-  #   secrets."nextcloud/admin_pass" = {};
-  # };
-  #
-  # services.nextcloud = {
-  #   enable = true;
-  #   package = pkgs.nextcloud31;
-  #   hostName = "localhost";
-  #   config.adminpassFile = config.sops.secrets."nextcloud/admin_pass".path;
-  #   config.dbtype = "sqlite";
-  # };
+  # Import secrets.
+  sops = {
+    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    defaultSopsFile = "${secrets}/sops.yaml";
+    secrets."nextcloud/admin_pass" = {};
+  };
+
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud31;
+    hostName = "localhost";
+    config.adminpassFile = config.sops.secrets."nextcloud/admin_pass".path;
+    config.dbtype = "pgsql";
+  };
 }
