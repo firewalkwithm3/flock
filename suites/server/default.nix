d362dac716e354703cf77cfbd90d69e03efcc8ae
Fern Garden <mail@fern.garden>
2025-07-23 16:21:24 +0800

    Update ornithologist pubkey

--- suites/server/default.nix
+++ suites/server/default.nix
@@ -18,7 +18,7 @@
   users.users.fern = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETPyuxUVEmYyEW6PVC6BXqkhULHd/RvMm8fMbYhjTMV fern@muskduck"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWeXKrVhdI6uJOaEGJ6FOBFLePiEmtdxQafrwYax+3M fern@ornithologist"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFy20lb9DWOZreqsY4ye4uu6VtKVfo4q7XD04d1VMLci fern@ornithologist"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMoJvPcUJDVVzO4dHROCFNlgJdDZSP5xyPx2s40zcx5QAAAABHNzaDo= YubiKey5NFC"
     ];
   };
