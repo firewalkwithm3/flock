2bcf8d9d69e8495f17b43a9ad1ad4c0875ae91de
Fern Garden <mail@fern.garden>
2025-07-23 16:19:30 +0800

    Add ornithologist filesystems

--- hosts/ornithologist/default.nix
+++ hosts/ornithologist/default.nix
@@ -1,4 +1,15 @@
 {pkgs, ...}: {
+  # Root filesystem.
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/11bdddf2-82b9-473d-b2ac-3b58f9f93ee7";
+      fsType = "ext4";
+    };
+
+  # Swap.
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/2fd2d201-14a2-45f8-abef-a1e57a509fe4"; }
+    ];
+
   # Install some packages.
   environment.systemPackages = with pkgs; [
     deploy-rs
