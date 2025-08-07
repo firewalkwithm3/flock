1b59563629c7350c52efb223855e8555bb71dd45
Fern Garden <mail@fern.garden>
2025-08-07 12:25:17 +0800

    Update flake.lock & ssh key

--- suites/server/default.nix
+++ suites/server/default.nix
@@ -20,7 +20,7 @@
   # Add authorized ssh pubkeys.
   users.users.fern = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETPyuxUVEmYyEW6PVC6BXqkhULHd/RvMm8fMbYhjTMV fern@muskduck"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNIujht3MTXQ6Ma5ADD6ko0SrL5U13qsni9Wcqli5LL fern@muskduck"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMoJvPcUJDVVzO4dHROCFNlgJdDZSP5xyPx2s40zcx5QAAAABHNzaDo= YubiKey5NFC"
     ];
   };
