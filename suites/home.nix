83bb9ac090044b24807c9ce19dd3d7c5fb21b2cc
Fern Garden <mail@fern.garden>
2025-08-10 16:29:35 +0800

    aria2 config

--- suites/home.nix
+++ suites/home.nix
@@ -4,4 +4,16 @@
     userEmail = "mail@fern.garden";
     userName = "Fern Garden";
   };
+
+  programs.aria2 = {
+    enable = true;
+    settings = {
+      max-concurrent-downloads = 5;
+      max-connection-per-server = 16;
+      min-split-size = "8M";
+      split = 32;
+      disk-cache = "64M";
+      file-allocation = "falloc";
+    };
+  };
 }
