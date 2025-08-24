fa8f67bc4b6361244c2cad0f12afe0f4c98919b6
Fern Garden <mail@fern.garden>
2025-08-24 21:28:03 +0800

    Disk paths

--- hosts/musicbrainz/default.nix
+++ hosts/musicbrainz/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
-  rootDisk = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi2";
-  rootPart = "/dev/disk/by-uuid/0804a690-0dbe-493d-9f2c-5db091569649";
+  rootDisk = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
+  rootPart = "/dev/disk/by-uuid/d90b9f44-42fb-4ccc-8b8d-05375d953742";
   dockerPart = "/dev/disk/by-uuid/0eb05c79-7765-4b7e-bf22-c3a53f516db5";
 in {
   boot.loader.grub.device = rootDisk;
