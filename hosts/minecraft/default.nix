20afd1b93e7c232f7d5c57e1f253edc519252c5c
Fern Garden <mail@fern.garden>
2025-08-24 22:35:48 +0800

    Finish minecraft

--- hosts/minecraft/default.nix
+++ hosts/minecraft/default.nix
@@ -1,6 +1,6 @@
 let
-  rootDisk = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi2";
-  rootPart = "f59330d9-0315-43c0-90a1-d9b13c6298f9";
+  rootDisk = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
+  rootPart = "/dev/disk/by-uuid/f59330d9-0315-43c0-90a1-d9b13c6298f9";
   dockerPart = "/dev/disk/by-uuid/3730e48a-8784-4c49-8692-473c9b4bc8c3";
 in {
   # Bootloader.
@@ -13,7 +13,7 @@ in {
   };
 
   # Docker data directory.
-  fileSystems."/home/fern/docker/data" = {
+  fileSystems."/home/fern/docker" = {
     device = dockerPart;
     fsType = "ext4";
   };
